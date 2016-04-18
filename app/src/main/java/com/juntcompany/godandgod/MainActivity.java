2d0a7f39a5462c562b40ae01926247d22f46b312
kiss9815 <kiss9815@naver.com>
2016-04-19 00:53:04 +0900

    4/18 대충 tab 하고 toolbar custom 만

--- app/src/main/java/com/juntcompany/godandgod/MainActivity.java
+++ app/src/main/java/com/juntcompany/godandgod/MainActivity.java
@@ -3,15 +3,22 @@ package com.juntcompany.godandgod;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ViewPager pager;
+    MainPagerAdapter pagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,17 +26,53 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ActionBar actionBar =  getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        View TitleView = getLayoutInflater().inflate(R.layout.toolbar_main, null);
+
+
+        actionBar.setCustomView(TitleView);
+
+        tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        pager = (ViewPager)findViewById(R.id.pager);
+
+        pagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(pager);
+        tabLayout.removeAllTabs();
+
+
+        tabLayout.addTab(tabLayout.newTab().setText("첫번째"));
+        tabLayout.addTab(tabLayout.newTab().setText("두번째"));
+        tabLayout.addTab(tabLayout.newTab().setText("세번째"));
+        tabLayout.addTab(tabLayout.newTab().setText("네번째"));
+        tabLayout.addTab(tabLayout.newTab().setText("다섯번째"));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onTabSelected(TabLayout.Tab tab) {
+                TabFragment tf = TabFragment.newInstance(tab.getText().toString());
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, tf)
+                        .commit();
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
-
-
-
+        TabFragment tf = TabFragment.newInstance("tab0");
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, tf)
+                .commit();
 
     }
 
