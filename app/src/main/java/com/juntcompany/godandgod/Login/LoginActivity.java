12b53a72482d1d02565f0c31268526b7fc4103d6
Jiseong <jiseong369@naver.com>
2016-04-27 19:41:40 +0900

    4/27 jiseong SignIn

--- app/src/main/java/com/juntcompany/godandgod/Login/LoginActivity.java
+++ app/src/main/java/com/juntcompany/godandgod/Login/LoginActivity.java
@@ -23,12 +23,12 @@ public class LoginActivity extends AppCompatActivity {
     public void loginActivityClick(View v) {
         EditText id = (EditText)findViewById(R.id.idText);
         EditText pw = (EditText)findViewById(R.id.passwordText);
-
+        Intent intent;
         switch (v.getId()) {
             case R.id.loginButton:
                 if(id.getText().toString().equals( "jiseong369")) {
                     if(pw.getText().toString().equals("gksvks12")) {
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent = new Intent(getApplicationContext(), MainActivity.class);
                         MainActivity.loginStatus = true;
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // 메인을 들어가면 메인 전에 실행했던 TASK 를 모두 삭제
                         startActivity(intent);
@@ -43,6 +43,15 @@ public class LoginActivity extends AppCompatActivity {
                 {
                     Toast.makeText(this, "ID 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
                 }
+                break;
+
+            case R.id.loginHelp:
+                intent = new Intent(getApplicationContext(), LoginHelpActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.signHelp:
+                intent = new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(intent);
                 break;
         }
     }
