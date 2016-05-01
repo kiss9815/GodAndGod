be46feb11a592cfdebebdf9b077e9a2116d351fb
goseunglyeol <tmdfuf>
2016-05-01 15:20:15 +0900

    5/1 phonefragment update

--- app/src/main/java/com/juntcompany/godandgod/Login/SignIn/SignInPhoneFragment.java
+++ app/src/main/java/com/juntcompany/godandgod/Login/SignIn/SignInPhoneFragment.java
@@ -5,14 +5,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.RecyclerView ;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.juntcompany.godandgod.R;
 
@@ -34,8 +40,10 @@ public class SignInPhoneFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_in_phone, container, false);
-        Button btn = (Button)view.findViewById(R.id.buttonNext);
+        Button btn = (Button) view.findViewById(R.id.buttonNext);
+
         SignInActivity.fNum = 2;
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,8 +53,23 @@ public class SignInPhoneFragment extends Fragment {
                 ft.commit();    //어떤 버튼이 눌리던 백스택에 해당 프래그먼트가 저장됨
             }
         });
-        ((SignInActivity) getActivity()).setActionTitle(Title);
-        return view;
-    }
+        ((SignInActivity) getActivity()).getApplicationContext();
 
+        String[] str = getResources().getStringArray(R.array.nation);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (getActivity().getApplication(), android.R.layout.simple_spinner_dropdown_item, str);
+        Spinner s = (Spinner) view.findViewById(R.id.spinner);
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                        @Override
+                                        public void onItemSelected(AdapterView<?> parent, View view,
+                                                                   int position, long id) { }
+                                        @Override
+                                        public void onNothingSelected(AdapterView<?> parent) {}
+
+                                    }
+        );
+
+        return view;
+
+    }
 }
