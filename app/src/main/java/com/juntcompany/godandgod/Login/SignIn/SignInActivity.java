914e9cefb44f38b3549038cc33479740c2dc6efb
goseunglyeol <tmdfuf>
2016-05-01 14:58:59 +0900

    5/1 phonefragment update

--- app/src/main/java/com/juntcompany/godandgod/Login/SignIn/SignInActivity.java
+++ app/src/main/java/com/juntcompany/godandgod/Login/SignIn/SignInActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView ;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
