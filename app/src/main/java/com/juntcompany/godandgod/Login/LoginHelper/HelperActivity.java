02ac4eeec83847e9716b665fa74a2692448c04ee
goseunglyeol <tmdfuf>
2016-05-03 17:20:41 +0900

    5/3 seoungryoul LoginHelper update

--- app/src/main/java/com/juntcompany/godandgod/Login/LoginHelper/HelperActivity.java
+++ app/src/main/java/com/juntcompany/godandgod/Login/LoginHelper/HelperActivity.java
@@ -1,20 +1,44 @@
 package com.juntcompany.godandgod.Login.LoginHelper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-
+import android.view.View;
+import com.juntcompany.godandgod.Login.SignIn.SignInActivity;
+import com.juntcompany.godandgod.Login.LoginHelper.HelperActivity;
+import com.juntcompany.godandgod.Login.SignIn.SignInChooseFragment;
 import com.juntcompany.godandgod.R;
 
 public class HelperActivity extends AppCompatActivity {
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_help);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Fragment f = new HelperMainFragment();//첫번째 프래그먼트를 설정
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, f);
+        ft.commit();
+    }
+
+    public void setActionTitle(String Title){
+        getSupportActionBar().setTitle(Title);
+    }
+
+    public void helperclock(View v){
+
+        switch (v.getId()){
+            case R.id.serachphone:
+                intent = new Intent(getApplicationContext(),HelperPhoneFindFragment.class);
+                startActivity(intent);
+                break;
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/juntcompany/godandgod/Login/LoginHelper/HelperMainFragment.java
@@ -0,0 +1,31 @@
+package com.juntcompany.godandgod.Login.LoginHelper;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.juntcompany.godandgod.R;
+
+
+public class HelperMainFragment extends Fragment {
+
+    private static final String Title = "휴대폰 번호";
+    public HelperMainFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_helper_main, container, false);
+    }
+
+}
