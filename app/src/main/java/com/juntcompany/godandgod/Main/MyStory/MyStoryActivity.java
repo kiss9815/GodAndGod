d8e73d8f321eb4b0c8b71d711d1c5dabb81d2892
Jiseong <jiseong369@naver.com>
2016-05-20 11:10:59 +0900

    5/20 jiseong merge all

--- app/src/main/java/com/juntcompany/godandgod/Main/MyStory/MyStoryActivity.java
+++ app/src/main/java/com/juntcompany/godandgod/Main/MyStory/MyStoryActivity.java
@@ -1,22 +1,52 @@
 package com.juntcompany.godandgod.Main.MyStory;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.juntcompany.godandgod.Main.Friend.FriendFragment;
+import com.juntcompany.godandgod.Main.Home.HomeFragment;
+import com.juntcompany.godandgod.Main.Live.LiveFragment;
+import com.juntcompany.godandgod.Main.Love.LoveFragment;
+import com.juntcompany.godandgod.Main.Video.VideoFragment;
 import com.juntcompany.godandgod.R;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MyStoryActivity extends AppCompatActivity {
+    int picAdd = 0;
+    private int TAKE_CAMERA = 1; // 카메라 리턴 코드값 설정
+    private int TAKE_GALLERY = 2; // 앨범선택에 대한 리턴 코드값 설정
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +59,74 @@ public class MyStoryActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("게시글");
         View titleView = getLayoutInflater().inflate(R.layout.toolbar_writestory, null);
         actionBar.setCustomView(titleView);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
-        EditText myStory = (EditText)findViewById(R.id.myStoryInput);
+        Button picAddBtn = (Button) findViewById(R.id.addpicBtn);
+        Button userAddBtn = (Button) findViewById(R.id.addUserBtn);
+        picAddBtn.setOnClickListener(new View.OnClickListener() {
+            ImageView pictureAdd = (ImageView) findViewById(R.id.myStoryPicture);
+
+            @Override
+            public void onClick(View v) {
+                if (picAdd == 0) {
+                    picAdd = 1;
+                    pictureAdd.setVisibility(View.VISIBLE);
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, TAKE_GALLERY);
+
+                } else if (picAdd == 1) {
+                    picAdd = 0;
+                    pictureAdd.setVisibility(View.GONE);
+                }
+            }
+        });
+        userAddBtn.setOnClickListener(new View.OnClickListener() {
+            ImageView pictureAdd = (ImageView) findViewById(R.id.myStoryPicture);
+
+            @Override
+            public void onClick(View v) {
+                if (picAdd == 0) {
+                    picAdd = 1;
+                    pictureAdd.setVisibility(View.VISIBLE);
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, TAKE_CAMERA);
+
+                } else if (picAdd == 1) {
+                    picAdd = 0;
+                    pictureAdd.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        final EditText myStory = (EditText) findViewById(R.id.myStoryInput);
+
+        myStory.setOnClickListener(new View.OnClickListener() {
+            ImageView hintImage = (ImageView) findViewById(R.id.hintImage);
+
+            @Override
+            public void onClick(View v) {
+                hintImage.setVisibility(View.GONE);
+                myStory.setHint("");
+            }
+        });
         myStory.addTextChangedListener(new TextWatcher() {
-            ImageView hintImage = (ImageView)findViewById(R.id.hintImage);
-            TextView InputOkStory = (TextView)findViewById(R.id.inputStoryOk);
-            EditText myStory = (EditText)findViewById(R.id.myStoryInput);
+            ImageView hintImage = (ImageView) findViewById(R.id.hintImage);
+            TextView InputOkStory = (TextView) findViewById(R.id.inputStoryOk);
+            EditText myStory = (EditText) findViewById(R.id.myStoryInput);
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                if(myStory.getText().length() == 0)
-                {
+                if (myStory.getText().length() == 0) {
                     hintImage.setVisibility(View.VISIBLE);
+                    myStory.setHint("          무슨 이야기를 나누고 싶나요?");
                     InputOkStory.setFocusable(false);
                     InputOkStory.setTextColor(Color.GRAY);
-                }
-                else
-                {
+
+                } else {
                     hintImage.setVisibility(View.GONE);
                     InputOkStory.setFocusable(true);
                     InputOkStory.setTextColor(Color.WHITE);
@@ -62,8 +143,9 @@ public class MyStoryActivity extends AppCompatActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
                 // 입력하기 전에
             }
-        });;
+        });
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -73,4 +155,97 @@ public class MyStoryActivity extends AppCompatActivity {
         }
         return true;
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == TAKE_CAMERA) {
+                if (data != null) {
+                    Log.e("Test", "result = " + data);
+                    Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                    if (thumbnail != null) {
+                        ImageView Imageview = (ImageView) findViewById(R.id.myStoryPicture);
+                        Imageview.setImageBitmap(thumbnail);
+                    }
+                }
+
+            } else if (requestCode == TAKE_GALLERY) {
+                if (data != null) {
+                    Log.e("Test", "result = " + data);
+
+                    Uri thumbnail = data.getData();
+                    try {
+                        Uri selectedImage = data.getData();
+
+                        Bitmap bp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+
+
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                        bp.compress(Bitmap.CompressFormat.JPEG, 80, baos);
+
+                        byte[] imageByte = baos.toByteArray();
+
+                        ImageView Iv = (ImageView) findViewById(R.id.myStoryPicture);
+
+                        Iv.setImageBitmap(BitmapFactory.decodeByteArray(imageByte, 0, imageByte.length));
+
+
+                        int len;
+
+                        final int size = 512;
+
+                        byte[] sendByte = new byte[size];
+
+                        ByteArrayInputStream bais = new ByteArrayInputStream(imageByte);
+
+
+                        sendByte[0] = 6;
+
+                        sendByte[1] = 26;
+
+                        sendByte[2] = 18;
+
+
+                        while ((len = bais.read(sendByte)) != -1) {
+
+                            if (len < 512) {
+
+                                byte[] EOF = new byte[len];
+
+                                for (int eof = 0; eof < EOF.length; eof++) {
+
+                                    EOF[eof] = sendByte[eof];
+
+                                }
+
+
+                            } else {
+
+
+                            }
+
+                        }
+
+
+                        sendByte[0] = 26;
+
+                        sendByte[1] = 6;
+
+                        sendByte[2] = 18;
+
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+        }
+    }
+
 }
